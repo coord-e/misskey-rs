100a03e55aaf3b7a5b130b09fbfcf116310501aa
coord.e <me@coord-e.com>
2020-08-20 11:06:02 +0000

    Add: Implement drive/ endpoint

--- misskey-api/src/api/drive.rs
+++ misskey-api/src/api/drive.rs
@@ -1,1 +1,32 @@
+use misskey_core::ApiRequest;
+use serde::{Deserialize, Serialize};
+
 pub mod folders;
+
+#[derive(Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Response {
+    pub capacity: u64,
+    pub usage: u64,
+}
+
+impl ApiRequest for Request {
+    type Response = Response;
+    const ENDPOINT: &'static str = "drive";
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Request;
+    use crate::test::{ClientExt, TestClient};
+
+    #[tokio::test]
+    async fn request() {
+        let mut client = TestClient::new();
+        client.test(Request {}).await;
+    }
+}
