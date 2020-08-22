6d51ddf4b49972cd7e337aba5dede8a2f5823a3e
coord.e <me@coord-e.com>
2020-08-22 19:50:09 +0000

    Add: Add tests that asserts errors are `Send` and `Sync`

--- misskey-websocket/src/error.rs
+++ misskey-websocket/src/error.rs
@@ -36,3 +36,20 @@ impl From<serde_json::Error> for Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::Error;
+
+    #[test]
+    fn test_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<Error>();
+    }
+
+    #[test]
+    fn test_sync() {
+        fn assert_send<T: Sync>() {}
+        assert_send::<Error>();
+    }
+}
