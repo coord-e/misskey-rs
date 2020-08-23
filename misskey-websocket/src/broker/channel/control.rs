1e694246b3db873c2a7dba122d655ed8856764b5
coord.e <me@coord-e.com>
2020-08-23 12:38:42 +0000

    Change: Consider when all control senders are dropped

--- misskey-websocket/src/broker/channel/control.rs
+++ misskey-websocket/src/broker/channel/control.rs
@@ -5,7 +5,7 @@ use crate::broker::model::{BrokerControl, SharedBrokerState};
 use crate::error::Result;
 
 use futures::channel::mpsc::{self, UnboundedReceiver, UnboundedSender};
-use futures::stream::Stream;
+use futures::stream::{FusedStream, Stream};
 
 /// Sender channel that the client uses to communicate with broker
 #[derive(Debug, Clone)]
@@ -36,7 +36,8 @@ impl ControlReceiver {
     pub fn try_recv(&mut self) -> Option<BrokerControl> {
         match self.0.try_next() {
             Ok(Some(x)) => Some(x),
-            Ok(None) => panic!("try_next returned None but we never closed the channel"),
+            // all control senders are dropped
+            Ok(None) => None,
             Err(_) => None,
         }
     }
@@ -46,6 +47,12 @@ impl Stream for ControlReceiver {
     type Item = BrokerControl;
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<BrokerControl>> {
         Pin::new(&mut self.0).poll_next(cx)
+    }
+}
+
+impl FusedStream for ControlReceiver {
+    fn is_terminated(&self) -> bool {
+        self.0.is_terminated()
     }
 }
 
