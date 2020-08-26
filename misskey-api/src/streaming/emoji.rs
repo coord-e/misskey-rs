08a7c57f4e5639e4db5067c9939586d0dbfb46ca
coord.e <me@coord-e.com>
2020-08-26 18:54:05 +0000

    Add: Add test case for `EmojiAddedEvent` broadcasting

--- misskey-api/src/streaming/emoji.rs
+++ misskey-api/src/streaming/emoji.rs
@@ -11,3 +11,26 @@ pub struct EmojiAddedEvent {
 impl misskey_core::streaming::BroadcastItem for EmojiAddedEvent {
     const TYPE: &'static str = "emojiAdded";
 }
+
+#[cfg(test)]
+mod tests {
+    use super::EmojiAddedEvent;
+    use crate::test::{websocket::TestClient, ClientExt};
+
+    use futures::{future, StreamExt};
+    use misskey_core::streaming::BroadcastClient;
+    use misskey_websocket::stream::Broadcast;
+
+    #[async_std::test]
+    async fn broadcast() {
+        let mut client = TestClient::new().await;
+        let url = client.avatar_url().await;
+
+        let mut stream: Broadcast<EmojiAddedEvent> = client.broadcast().await.unwrap();
+
+        future::join(client.admin.add_emoji_from_url(url), async {
+            stream.next().await.unwrap().unwrap()
+        })
+        .await;
+    }
+}
