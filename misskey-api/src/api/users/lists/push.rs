a2e5ccc5715938251a1e8ff988cd3ba0a696bd79
coord.e <me@coord-e.com>
2020-08-26 21:32:04 +0000

    Add: Add missing test case for users/lists/push

--- misskey-api/src/api/users/lists/push.rs
+++ misskey-api/src/api/users/lists/push.rs
@@ -13,3 +13,27 @@ impl misskey_core::Request for Request {
     type Response = ();
     const ENDPOINT: &'static str = "users/lists/push";
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Request;
+    use crate::test::{ClientExt, TestClient};
+
+    #[tokio::test]
+    async fn request() {
+        let mut client = TestClient::new();
+        let (user, _) = client.admin.create_user().await;
+        let list = client
+            .test(crate::api::users::lists::create::Request {
+                name: "test".to_string(),
+            })
+            .await;
+
+        client
+            .test(Request {
+                list_id: list.id,
+                user_id: user.id,
+            })
+            .await;
+    }
+}
