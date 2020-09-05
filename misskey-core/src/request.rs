c3c37e131a173252a39c12cffcc54320a5e493c9
coord.e <me@coord-e.com>
2020-09-05 12:43:42 +0000

    Add: Add some obvious instances of requests

--- misskey-core/src/request.rs
+++ misskey-core/src/request.rs
@@ -6,4 +6,32 @@ pub trait Request: Serialize {
     const ENDPOINT: &'static str;
 }
 
+impl<R: ?Sized> Request for &'_ R
+where
+    R: Request,
+{
+    type Response = R::Response;
+    const ENDPOINT: &'static str = R::ENDPOINT;
+}
+
+impl<R: ?Sized> Request for &'_ mut R
+where
+    R: Request,
+{
+    type Response = R::Response;
+    const ENDPOINT: &'static str = R::ENDPOINT;
+}
+
+impl<R: ?Sized> Request for Box<R>
+where
+    R: Request,
+{
+    type Response = R::Response;
+    const ENDPOINT: &'static str = R::ENDPOINT;
+}
+
 pub trait UploadFileRequest: Request {}
+
+impl<R: ?Sized> UploadFileRequest for &'_ R where R: UploadFileRequest {}
+impl<R: ?Sized> UploadFileRequest for &'_ mut R where R: UploadFileRequest {}
+impl<R: ?Sized> UploadFileRequest for Box<R> where R: UploadFileRequest {}
--- misskey-core/src/streaming/api.rs
+++ misskey-core/src/streaming/api.rs
@@ -13,6 +13,36 @@ pub trait ConnectChannelRequest: Serialize {
     const NAME: &'static str;
 }
 
+impl<R: ?Sized> ConnectChannelRequest for &'_ R
+where
+    R: ConnectChannelRequest,
+{
+    type Incoming = R::Incoming;
+    type Outgoing = R::Outgoing;
+
+    const NAME: &'static str = R::NAME;
+}
+
+impl<R: ?Sized> ConnectChannelRequest for &'_ mut R
+where
+    R: ConnectChannelRequest,
+{
+    type Incoming = R::Incoming;
+    type Outgoing = R::Outgoing;
+
+    const NAME: &'static str = R::NAME;
+}
+
+impl<R: ?Sized> ConnectChannelRequest for Box<R>
+where
+    R: ConnectChannelRequest,
+{
+    type Incoming = R::Incoming;
+    type Outgoing = R::Outgoing;
+
+    const NAME: &'static str = R::NAME;
+}
+
 pub trait SubNoteEvent: DeserializeOwned + 'static {}
 
 pub trait BroadcastEvent: DeserializeOwned + 'static {
