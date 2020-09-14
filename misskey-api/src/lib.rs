c66c9bb68a7857762d10488923ab2da5bdf27984
coord.e <me@coord-e.com>
2020-09-14 17:04:31 +0000

    Fix: Fix some words

--- misskey-api/src/lib.rs
+++ misskey-api/src/lib.rs
@@ -1,4 +1,4 @@
-//! API datatypes of Misskey, including requests/responses of [endpoints][`endpoint`] and messages on [channels][`streaming::channel`], for [misskey-rs](https://docs.rs/misskey).
+//! API bindings of [Misskey](https://github.com/syuilo/misskey), including requests/responses of [endpoints][`endpoint`] and messages on [channels][`streaming::channel`], for [misskey-rs](https://docs.rs/misskey).
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub mod endpoint;
