8d95f2d08e0da8d118a46c3c3a35d4f95e5e18bd
coord.e <me@coord-e.com>
2021-04-07 12:10:08 +0000

    Fix: Deal with new clippy lints

--- misskey-util/src/error.rs
+++ misskey-util/src/error.rs
@@ -7,7 +7,7 @@ pub enum Error<E> {
     /// Errors from underlying client, namely `E`.
     Client(E),
     /// Errors from Misskey API.
-    API(ApiError),
+    Api(ApiError),
     /// IO Errors from some high-level API.
     Io(std::io::Error),
 }
@@ -16,7 +16,7 @@ impl<E: std::error::Error> std::error::Error for Error<E> {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Error::Client(err) => err.source(),
-            Error::API(err) => Some(err),
+            Error::Api(err) => Some(err),
             Error::Io(err) => err.source(),
         }
     }
@@ -26,7 +26,7 @@ impl<E: std::error::Error> std::fmt::Display for Error<E> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Client(err) => Display::fmt(err, f),
-            Error::API(_) => write!(f, "Misskey API returned an error"),
+            Error::Api(_) => write!(f, "Misskey API returned an error"),
             Error::Io(err) => Display::fmt(err, f),
         }
     }
@@ -36,7 +36,7 @@ impl<E: std::error::Error> Debug for Error<E> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Client(err) => f.debug_tuple("Client").field(&err).finish(),
-            Error::API(err) => f.debug_tuple("API").field(&err).finish(),
+            Error::Api(err) => f.debug_tuple("Api").field(&err).finish(),
             Error::Io(err) => f.debug_tuple("Io").field(&err).finish(),
         }
     }
@@ -44,7 +44,7 @@ impl<E: std::error::Error> Debug for Error<E> {
 
 impl<E> From<ApiError> for Error<E> {
     fn from(err: ApiError) -> Self {
-        Error::API(err)
+        Error::Api(err)
     }
 }
 
