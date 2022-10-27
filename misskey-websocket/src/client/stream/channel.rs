75ec137fb9268d6092c5d2387feafc1c80d0d08f
coord_e <me@coord-e.com>
2022-10-28 00:44:06 +0900

    refactor: remove futures::executor uses in misskey-websocket
    This causes the streams to no longer block when it is dropped.

--- misskey-websocket/src/client/stream/channel.rs
+++ misskey-websocket/src/client/stream/channel.rs
@@ -12,8 +12,9 @@ use crate::broker::{
 use crate::error::{Error, Result};
 use crate::model::ChannelId;
 
+#[cfg(feature = "async-std-runtime")]
+use async_std::task;
 use futures::{
-    executor,
     future::BoxFuture,
     sink::{Sink, SinkExt},
     stream::{FusedStream, Stream, StreamExt},
@@ -22,84 +23,122 @@ use log::{info, warn};
 use misskey_core::streaming::ConnectChannelRequest;
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::Value;
+#[cfg(feature = "tokio-runtime")]
+use tokio::task;
 
-/// Stream for the [`channel`][`crate::WebSocketClient::channel`] method.
-#[must_use = "streams do nothing unless polled"]
-pub struct Channel<I, O> {
+pub struct ChannelInner {
     id: ChannelId,
     broker_tx: ControlSender,
     response_rx: ResponseStreamReceiver<Value>,
     is_terminated: bool,
-    _marker: PhantomData<fn() -> (I, O)>,
 }
 
-impl<I, O> Debug for Channel<I, O> {
+impl Debug for ChannelInner {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("Channel")
+        f.debug_struct("ChannelInner")
             .field("id", &self.id)
             .field("is_terminated", &self.is_terminated)
             .finish()
     }
 }
 
-impl<I, O> Channel<I, O>
-where
-    I: DeserializeOwned + 'static,
-    O: Serialize + 'static,
-{
-    // We can't use return-position `impl Trait` syntax here because it assumes all type parameters (i.e. `R`)
-    // are "in scope" of (hidden) returned type, and they indirectly brings (unmentioned) lifetime of `R`.
-    // Thus we can't express our returned (anonymous) `Future` without `BoxFuture` for now
-    // because it is actually independent from `req: R` argument.
-    pub(crate) fn connect<R>(
-        req: R,
+impl Stream for ChannelInner {
+    type Item = Result<Value>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Value>>> {
+        if self.is_terminated {
+            return Poll::Ready(None);
+        }
+
+        self.response_rx.poll_next_unpin(cx)
+    }
+}
+
+impl Sink<Value> for ChannelInner {
+    type Error = Error;
+
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
+        self.broker_tx.poll_ready_unpin(cx)
+    }
+
+    fn start_send(mut self: Pin<&mut Self>, item: Value) -> Result<()> {
+        let item = BrokerControl::Channel {
+            id: self.id,
+            message: item,
+        };
+        self.broker_tx.start_send_unpin(item)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
+        self.broker_tx.poll_flush_unpin(cx)
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
+        self.broker_tx.poll_close_unpin(cx)
+    }
+}
+
+impl FusedStream for ChannelInner {
+    fn is_terminated(&self) -> bool {
+        self.is_terminated
+    }
+}
+
+impl Drop for ChannelInner {
+    fn drop(&mut self) {
+        if self.is_terminated {
+            return;
+        }
+
+        let mut broker_tx = self.broker_tx.clone();
+        let id = self.id;
+        task::spawn(async move {
+            // If the broker or websocket connection is dead, we don't need to unsubscribe anyway
+            // because the client can't be used anymore.
+            if let Err(e) = broker_tx.send(BrokerControl::Disconnect { id }).await {
+                warn!(
+                    "Channel::disconnect failed in Drop::drop (ignored): {:?}",
+                    e
+                );
+            }
+        });
+    }
+}
+
+impl ChannelInner {
+    async fn connect(
+        name: &'static str,
+        serialized_req: Value,
         mut broker_tx: ControlSender,
         state: SharedBrokerState,
-    ) -> BoxFuture<'static, Result<Channel<I, O>>>
-    where
-        R: ConnectChannelRequest<Incoming = I, Outgoing = O>,
-    {
+    ) -> Result<ChannelInner> {
         let id = ChannelId::uuid();
 
         let (response_tx, response_rx) = response_stream_channel(SharedBrokerState::clone(&state));
         let (pong_tx, pong_rx) = channel_pong_channel(state);
 
-        // limit the use of `R` to the outside of `async`
-        // in order not to require `Send` on `R`
-        let serialized_req = serde_json::to_value(req);
-
-        Box::pin(async move {
-            broker_tx
-                .send(BrokerControl::Connect {
-                    id,
-                    name: R::NAME,
-                    params: serialized_req?,
-                    sender: response_tx,
-                    pong: pong_tx,
-                })
-                .await?;
-
-            // wait for `connected` pong message from server
-            pong_rx.recv().await?;
-
-            Ok(Channel {
+        broker_tx
+            .send(BrokerControl::Connect {
                 id,
-                broker_tx,
-                response_rx,
-                is_terminated: false,
-                _marker: PhantomData,
+                name,
+                params: serialized_req,
+                sender: response_tx,
+                pong: pong_tx,
             })
+            .await?;
+
+        // wait for `connected` pong message from server
+        pong_rx.recv().await?;
+
+        Ok(ChannelInner {
+            id,
+            broker_tx,
+            response_rx,
+            is_terminated: false,
         })
     }
-}
 
-impl<I, O> Channel<I, O> {
-    /// Disconnect from the channel.
-    ///
-    /// After this call, the stream is no longer available (terminated), i.e. [`StreamExt::next`] returns [`None`].
-    /// If you call [`disconnect`][`Channel::disconnect`] on a terminated stream, it will simply
-    /// be ignored (with log message if logging is enabled).
-    pub async fn disconnect(&mut self) -> Result<()> {
+    async fn disconnect(&mut self) -> Result<()> {
         if self.is_terminated {
             info!("disconnecting from already terminated Channel, skipping");
             return Ok(());
@@ -115,6 +154,54 @@ impl<I, O> Channel<I, O> {
     }
 }
 
+/// Stream for the [`channel`][`crate::WebSocketClient::channel`] method.
+#[must_use = "streams do nothing unless polled"]
+#[derive(Debug)]
+pub struct Channel<I, O> {
+    inner: ChannelInner,
+    _marker: PhantomData<fn() -> (I, O)>,
+}
+
+impl<I, O> Channel<I, O>
+where
+    I: DeserializeOwned + 'static,
+    O: Serialize + 'static,
+{
+    // We can't use return-position `impl Trait` syntax here because it assumes all type parameters (i.e. `R`)
+    // are "in scope" of (hidden) returned type, and they indirectly brings (unmentioned) lifetime of `R`.
+    // Thus we can't express our returned (anonymous) `Future` without `BoxFuture` for now
+    // because it is actually independent from `req: R` argument.
+    pub(crate) fn connect<R>(
+        req: R,
+        broker_tx: ControlSender,
+        state: SharedBrokerState,
+    ) -> BoxFuture<'static, Result<Channel<I, O>>>
+    where
+        R: ConnectChannelRequest<Incoming = I, Outgoing = O>,
+    {
+        let req = serde_json::to_value(req);
+        Box::pin(async move {
+            ChannelInner::connect(R::NAME, req?, broker_tx, state)
+                .await
+                .map(|inner| Channel {
+                    inner,
+                    _marker: PhantomData,
+                })
+        })
+    }
+}
+
+impl<I, O> Channel<I, O> {
+    /// Disconnect from the channel.
+    ///
+    /// After this call, the stream is no longer available (terminated), i.e. [`StreamExt::next`] returns [`None`].
+    /// If you call [`disconnect`][`Channel::disconnect`] on a terminated stream, it will simply
+    /// be ignored (with log message if logging is enabled).
+    pub async fn disconnect(&mut self) -> Result<()> {
+        self.inner.disconnect().await
+    }
+}
+
 impl<I, O> Stream for Channel<I, O>
 where
     I: DeserializeOwned,
@@ -122,14 +209,9 @@ where
     type Item = Result<I>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<I>>> {
-        if self.is_terminated {
-            return Poll::Ready(None);
-        }
-
-        match futures::ready!(self.response_rx.poll_next_unpin(cx)?) {
-            None => Poll::Ready(None),
-            Some(v) => Poll::Ready(Some(Ok(serde_json::from_value(v)?))),
-        }
+        let result = futures::ready!(self.inner.poll_next_unpin(cx))
+            .map(|res| res.and_then(|v| serde_json::from_value(v).map_err(Into::into)));
+        Poll::Ready(result)
     }
 }
 
@@ -140,23 +222,19 @@ where
     type Error = Error;
 
     fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
-        self.broker_tx.poll_ready_unpin(cx)
+        self.inner.poll_ready_unpin(cx)
     }
 
     fn start_send(mut self: Pin<&mut Self>, item: O) -> Result<()> {
-        let item = BrokerControl::Channel {
-            id: self.id,
-            message: serde_json::to_value(item)?,
-        };
-        self.broker_tx.start_send_unpin(item)
+        self.inner.start_send_unpin(serde_json::to_value(item)?)
     }
 
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
-        self.broker_tx.poll_flush_unpin(cx)
+        self.inner.poll_flush_unpin(cx)
     }
 
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<()>> {
-        self.broker_tx.poll_close_unpin(cx)
+        self.inner.poll_close_unpin(cx)
     }
 }
 
@@ -165,25 +243,6 @@ where
     I: DeserializeOwned,
 {
     fn is_terminated(&self) -> bool {
-        self.is_terminated
-    }
-}
-
-impl<I, O> Drop for Channel<I, O> {
-    fn drop(&mut self) {
-        if self.is_terminated {
-            return;
-        }
-
-        executor::block_on(async {
-            // If the broker or websocket connection is dead, we don't need to unsubscribe anyway
-            // because the client can't be used anymore.
-            if let Err(e) = self.disconnect().await {
-                warn!(
-                    "Channel::disconnect failed in Drop::drop (ignored): {:?}",
-                    e
-                );
-            }
-        });
+        self.inner.is_terminated()
     }
 }
--- misskey-websocket/src/client/stream/sub_note.rs
+++ misskey-websocket/src/client/stream/sub_note.rs
@@ -10,40 +10,79 @@ use crate::broker::{
 use crate::error::Result;
 use crate::model::SubNoteId;
 
+#[cfg(feature = "async-std-runtime")]
+use async_std::task;
 use futures::{
-    executor,
     sink::SinkExt,
     stream::{FusedStream, Stream, StreamExt},
 };
 use log::{info, warn};
 use misskey_core::streaming::SubNoteEvent;
 use serde_json::Value;
+#[cfg(feature = "tokio-runtime")]
+use tokio::task;
 
-/// Stream for the [`subnote`][`crate::WebSocketClient::subnote`] method.
-#[must_use = "streams do nothing unless polled"]
-pub struct SubNote<E> {
+pub struct SubNoteInner {
     id: SubNoteId,
     broker_tx: ControlSender,
     response_rx: ResponseStreamReceiver<Value>,
     is_terminated: bool,
-    _marker: PhantomData<fn() -> E>,
 }
 
-impl<E> Debug for SubNote<E> {
+impl Debug for SubNoteInner {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("SubNote")
+        f.debug_struct("SubNoteInner")
             .field("id", &self.id)
             .field("is_terminated", &self.is_terminated)
             .finish()
     }
 }
 
-impl<E> SubNote<E> {
-    pub(crate) async fn subscribe(
+impl Stream for SubNoteInner {
+    type Item = Result<Value>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Value>>> {
+        if self.is_terminated {
+            return Poll::Ready(None);
+        }
+
+        self.response_rx.poll_next_unpin(cx)
+    }
+}
+
+impl FusedStream for SubNoteInner {
+    fn is_terminated(&self) -> bool {
+        self.is_terminated
+    }
+}
+
+impl Drop for SubNoteInner {
+    fn drop(&mut self) {
+        if self.is_terminated {
+            return;
+        }
+
+        let mut broker_tx = self.broker_tx.clone();
+        let id = self.id.clone();
+        task::spawn(async move {
+            // If the broker connection is dead, we don't need to unsubscribe anyway
+            // because the client can't be used anymore.
+            if let Err(e) = broker_tx.send(BrokerControl::UnsubNote { id }).await {
+                warn!(
+                    "SubNote::unsubscribe failed in Drop::drop (ignored): {:?}",
+                    e
+                );
+            }
+        });
+    }
+}
+
+impl SubNoteInner {
+    async fn subscribe(
         id: SubNoteId,
         mut broker_tx: ControlSender,
         state: SharedBrokerState,
-    ) -> Result<SubNote<E>> {
+    ) -> Result<SubNoteInner> {
         let (response_tx, response_rx) = response_stream_channel(state);
         broker_tx
             .send(BrokerControl::SubNote {
@@ -52,21 +91,15 @@ impl<E> SubNote<E> {
             })
             .await?;
 
-        Ok(SubNote {
+        Ok(SubNoteInner {
             id,
             broker_tx,
             response_rx,
             is_terminated: false,
-            _marker: PhantomData,
         })
     }
 
-    /// Stop this subscription.
-    ///
-    /// After this call, the stream is no longer available (terminated), i.e. [`StreamExt::next`] returns [`None`].
-    /// If you call [`unsubscribe`][`SubNote::unsubscribe`] on a terminated stream, it will simply
-    /// be ignored (with log message if logging is enabled).
-    pub async fn unsubscribe(&mut self) -> Result<()> {
+    async fn unsubscribe(&mut self) -> Result<()> {
         if self.is_terminated {
             info!("unsubscribing already terminated SubNote, skipping");
             return Ok(());
@@ -84,6 +117,38 @@ impl<E> SubNote<E> {
     }
 }
 
+/// Stream for the [`subnote`][`crate::WebSocketClient::subnote`] method.
+#[must_use = "streams do nothing unless polled"]
+#[derive(Debug)]
+pub struct SubNote<E> {
+    inner: SubNoteInner,
+    _marker: PhantomData<fn() -> E>,
+}
+
+impl<E> SubNote<E> {
+    pub(crate) async fn subscribe(
+        id: SubNoteId,
+        broker_tx: ControlSender,
+        state: SharedBrokerState,
+    ) -> Result<SubNote<E>> {
+        SubNoteInner::subscribe(id, broker_tx, state)
+            .await
+            .map(|inner| SubNote {
+                inner,
+                _marker: PhantomData,
+            })
+    }
+
+    /// Stop this subscription.
+    ///
+    /// After this call, the stream is no longer available (terminated), i.e. [`StreamExt::next`] returns [`None`].
+    /// If you call [`unsubscribe`][`SubNote::unsubscribe`] on a terminated stream, it will simply
+    /// be ignored (with log message if logging is enabled).
+    pub async fn unsubscribe(&mut self) -> Result<()> {
+        self.inner.unsubscribe().await
+    }
+}
+
 impl<E> Stream for SubNote<E>
 where
     E: SubNoteEvent,
@@ -91,14 +156,9 @@ where
     type Item = Result<E>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<E>>> {
-        if self.is_terminated {
-            return Poll::Ready(None);
-        }
-
-        match futures::ready!(self.response_rx.poll_next_unpin(cx)?) {
-            None => Poll::Ready(None),
-            Some(v) => Poll::Ready(Some(Ok(serde_json::from_value(v)?))),
-        }
+        let result = futures::ready!(self.inner.poll_next_unpin(cx))
+            .map(|res| res.and_then(|v| serde_json::from_value(v).map_err(Into::into)));
+        Poll::Ready(result)
     }
 }
 
@@ -107,25 +167,6 @@ where
     E: SubNoteEvent,
 {
     fn is_terminated(&self) -> bool {
-        self.is_terminated
-    }
-}
-
-impl<E> Drop for SubNote<E> {
-    fn drop(&mut self) {
-        if self.is_terminated {
-            return;
-        }
-
-        executor::block_on(async {
-            // If the broker connection is dead, we don't need to unsubscribe anyway
-            // because the client can't be used anymore.
-            if let Err(e) = self.unsubscribe().await {
-                warn!(
-                    "SubNote::unsubscribe failed in Drop::drop (ignored): {:?}",
-                    e
-                );
-            }
-        });
+        self.inner.is_terminated()
     }
 }
