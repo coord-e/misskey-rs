ff3eada1cf3e82602da2331f9418400d31b02958
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-04-17 01:05:27 +0900

    Add: Support v12.111.0

--- misskey-api/src/endpoint/admin/show_user.rs
+++ misskey-api/src/endpoint/admin/show_user.rs
@@ -1,7 +1,16 @@
-use crate::model::{drive::DriveFile, id::Id, user::User};
+#[cfg(feature = "12-111-0")]
+use std::collections::HashMap;
 
+#[cfg(not(feature = "12-111-0"))]
+use crate::model::drive::DriveFile;
+use crate::model::{id::Id, user::User};
+#[cfg(feature = "12-111-0")]
+use crate::model::{notification::NotificationType, signin::Signin, user::IntegrationValue};
+
+#[cfg(not(feature = "12-111-0"))]
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+#[cfg(not(feature = "12-111-0"))]
 use url::Url;
 
 #[derive(Serialize, Debug, Clone)]
@@ -10,6 +19,8 @@ pub struct Request {
     pub user_id: Id<User>,
 }
 
+#[cfg(not(feature = "12-111-0"))]
+#[cfg_attr(docsrs, doc(cfg(not(feature = "12-111-0"))))]
 #[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Response {
@@ -55,6 +66,42 @@ pub struct Response {
     pub token: Option<String>,
 }
 
+#[cfg(feature = "12-111-0")]
+#[cfg_attr(docsrs, doc(cfg(feature = "12-111-0")))]
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Response {
+    #[serde(default)]
+    pub email: Option<String>,
+    #[serde(default)]
+    pub email_verified: Option<bool>,
+    #[serde(default)]
+    pub auto_accept_followed: Option<bool>,
+    #[serde(default)]
+    pub no_crawle: Option<bool>,
+    #[serde(default)]
+    pub always_mark_nsfw: Option<bool>,
+    #[serde(default)]
+    pub careful_bot: Option<bool>,
+    #[serde(default)]
+    pub inject_featured_note: Option<bool>,
+    #[serde(default)]
+    pub receive_announcement_email: Option<bool>,
+    #[serde(default)]
+    pub integrations: Option<HashMap<String, IntegrationValue>>,
+    #[serde(default)]
+    pub muted_words: Option<Vec<Vec<String>>>,
+    #[serde(default)]
+    pub muted_instances: Option<Vec<String>>,
+    #[serde(default)]
+    pub muting_notification_types: Option<Vec<NotificationType>>,
+    pub is_moderator: bool,
+    pub is_silenced: bool,
+    pub is_suspended: bool,
+    #[serde(default)]
+    pub signins: Option<Vec<Signin>>,
+}
+
 impl misskey_core::Request for Request {
     type Response = Response;
     const ENDPOINT: &'static str = "admin/show-user";
@@ -71,5 +118,17 @@ mod tests {
         let user = client.user.me().await;
 
         client.admin.test(Request { user_id: user.id }).await;
+    }
+
+    #[tokio::test]
+    async fn request_moderator() {
+        let client = TestClient::new();
+        let user_id = client.user.me().await.id;
+        client
+            .admin
+            .test(crate::endpoint::admin::moderators::add::Request { user_id })
+            .await;
+
+        client.user.test(Request { user_id }).await;
     }
 }
--- misskey-api/src/endpoint/notifications/read.rs
+++ misskey-api/src/endpoint/notifications/read.rs
@@ -13,9 +13,25 @@ impl misskey_core::Request for Request {
     const ENDPOINT: &'static str = "notifications/read";
 }
 
+#[cfg(feature = "12-111-0")]
+#[cfg_attr(docsrs, doc(cfg(feature = "12-111-0")))]
+#[derive(Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestWithNotificationIds {
+    pub notification_ids: Vec<Id<Notification>>,
+}
+
+#[cfg(feature = "12-111-0")]
+impl misskey_core::Request for RequestWithNotificationIds {
+    type Response = ();
+    const ENDPOINT: &'static str = "notifications/read";
+}
+
 #[cfg(test)]
 mod tests {
     use super::Request;
+    #[cfg(feature = "12-111-0")]
+    use super::RequestWithNotificationIds;
     use crate::test::{ClientExt, TestClient};
 
     #[tokio::test]
@@ -43,6 +59,35 @@ mod tests {
             .admin
             .test(Request {
                 notification_id: notification.unwrap().id,
+            })
+            .await;
+    }
+
+    #[cfg(feature = "12-111-0")]
+    #[tokio::test]
+    async fn request_with_notification_ids() {
+        let client = TestClient::new();
+        client
+            .admin
+            .test(crate::endpoint::notifications::create::Request {
+                body: "hi".to_string(),
+                header: None,
+                icon: None,
+            })
+            .await;
+
+        let notifications = client
+            .admin
+            .test(crate::endpoint::i::notifications::Request::default())
+            .await;
+
+        client
+            .admin
+            .test(RequestWithNotificationIds {
+                notification_ids: notifications
+                    .iter()
+                    .map(|notification| notification.id)
+                    .collect(),
             })
             .await;
     }
