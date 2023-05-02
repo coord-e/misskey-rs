064f8807005c36f712f06d2f49cbdd198da77071
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-05-03 04:47:25 +0900

    CI: Update scripts to be compatible with Misskey v13.0.0

--- ci/testenv/get-token/get-token.sh
+++ ci/testenv/get-token/get-token.sh
@@ -19,9 +19,55 @@ USER_DATA=$(
 USER_TOKEN=$(echo "$USER_DATA" | jq -r .token)
 USER_ID=$(echo "$USER_DATA" | jq -r .id)
 
+# endpoint `admin/moderators/add` is removed in Misskey v13.0.0
 curl -fsS -XPOST -H 'Content-Type: application/json'            \
   --data '{"i":"'"${ADMIN_TOKEN}"'","userId":"'"${USER_ID}"'"}' \
-  http://web:3000/api/admin/moderators/add || exit 1
+  http://web:3000/api/admin/moderators/add ||
+(
+  # create and assign moderator role
+  ROLE_ID=$(
+    curl -fsS -XPOST -H 'Content-Type: application/json' \
+      --data '{
+        "i": "'"${ADMIN_TOKEN}"'",
+        "name": "Moderator",
+        "description": "",
+        "color": "",
+        "target": "manual",
+        "condFormula": {},
+        "isPublic": false,
+        "isModerator": true,
+        "isAdministrator": false,
+        "canEditMembersByModerator": false,
+        "policies": {
+          "pinLimit": {
+            "value": 1000,
+            "priority": 0,
+            "useDefault": false
+          },
+          "clipLimit": {
+            "value": 1000,
+            "priority": 0,
+            "useDefault": false
+          },
+          "antennaLimit": {
+            "value": 1000,
+            "priority": 0,
+            "useDefault": false
+          },
+          "userListLimit": {
+            "value": 1000,
+            "priority": 0,
+            "useDefault": false
+          }
+        }
+      }' \
+    http://web:3000/api/admin/roles/create | jq -r .id \
+  ) &&
+  curl -fsS -XPOST -H 'Content-Type: application/json' \
+    --data '{"i":"'"${ADMIN_TOKEN}"'", "roleId": "'"${ROLE_ID}"'", "userId":"'"${USER_ID}"'"}' \
+    http://web:3000/api/admin/roles/assign
+) ||
+exit 1
 
 echo "::set-output name=admin_token::$ADMIN_TOKEN"
 echo "::set-output name=user_token::$USER_TOKEN"
